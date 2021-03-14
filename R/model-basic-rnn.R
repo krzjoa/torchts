02bac9f5f1de25ca00da4e94754b239b3af611c0
Krzysztof Joachimiak <kjoa.92@gmail.com>
2021-03-14 22:49:11 +0100

    nn_recurrent

--- R/model-basic-rnn.R
+++ R/model-basic-rnn.R
@@ -19,47 +19,20 @@
 #' catch <- function(optim){browser()}
 #' catch(optim_adam(lr = 0.001))
 #'
-#' model <- basic_rnn_fit(x, y, key = "item_id", index = "date", n_epochs = 1000)
+#' model <- recurrent_fit(x, y, key = "item_id", index = "date", n_epochs = 1000)
 #' fcast <- predict_basic_rnn_impl(model, x, key = "item_id", index = "date")
 #' View(bind_cols(y, fcast))
 #'
 #' Idea:
 #' - rnn_reg for parsnip
 #' - ts_rnn for fable (ts prefix for fable)
-basic_rnn_fit <- function(x, y, key, index, categorical_features = NULL,
+recurrent_fit <- function(x, y, key, index, categorical_features = NULL,
                           backward = NULL, optim = optim_adam(), batch_size = 1,
                           n_epochs = 10, loss_fn = nnf_mse_loss, plugins = NULL){
 
-  #' Search for factor or character variables
-  #' TODO: add messages, when factors o character variables detected (not mentioned in categorical_features)
-  #' TOOD: use data.table?
-  features             <- base::setdiff(colnames(x), c(key, index))
-  factor_features      <- features[sapply(x, is.factor)]
-  character_features   <- features[sapply(x, is.character)]
-  categorical_features <- c(categorical_features, factor_features, character_features)
-  categorical_features <- unique(categorical_features)
-
-  if (!is.null(backward)) {
-    warning("Backward is not implemented yet!")
-  }
-
-  #' Categorical features
-  #' These features have to have integer values, so we choose `torch_long()`
-  #' It's because module `nn_embedding` treats each categorical column as a vector
-  #' of indices.
-  if (!is.null(categorical_features)) {
-    X_tensor_cat <-
-      x %>%
-      select(!!key, !!index, !!categorical_features) %>%
-      arrange(!!key, !!index) %>%
-      as_tensor(!!key, !!index, dtype = torch_long())
-  }
-
-  X_tensor_numeric <-
-    x %>%
-    select(-!!categorical_features) %>%
-    arrange(!!key, !!index) %>%
-    as_tensor(!!key, !!index, dtype = torch_float())
+  input_tensors    <- resolve_data(x, key, index, categorical_features)
+  X_tensor_numeric <- input_tensors[[1]]
+  X_tensor_cat     <- input_tensors[[2]]
 
   # Input size
   n_features <-
@@ -75,7 +48,7 @@ basic_rnn_fit <- function(x, y, key, index, categorical_features = NULL,
     ceiling(dict_sizes ** 0.25)
 
   # Creating a neural network
-  neural_network <- basic_rnn(
+  neural_network <- nn_recurrent(
     fwd_input_size  = n_features,
     fwd_numeric_input =  tail(dim(X_tensor_numeric), 1),
     fwd_output_size = 3,
@@ -90,7 +63,6 @@ basic_rnn_fit <- function(x, y, key, index, categorical_features = NULL,
 
   # Train neural network
   # TODO: prepare plugins
-
   for (epoch in seq(n_epochs)) {
     neural_network$zero_grad()
     fcast <- neural_network(X_tensor_cat, X_tensor_numeric)
@@ -113,34 +85,11 @@ basic_rnn_fit <- function(x, y, key, index, categorical_features = NULL,
   )
 }
 
-predict_basic_rnn_impl <- function(obj, new_data, key, index, categorical_features = NULL){
-  #' Search for factor or character variables
-  #' TODO: add messages, when factors o character variables detected (not mentioned in categorical_features)
-  #' TOOD: use data.table?
-  features             <- base::setdiff(colnames(new_data), c(key, index))
-  factor_features      <- features[sapply(new_data, is.factor)]
-  character_features   <- features[sapply(new_data, is.character)]
-  categorical_features <- c(categorical_features, factor_features, character_features)
-  categorical_features <- unique(categorical_features)
 
-  #' Categorical features
-  #' These features have to have integer values, so we choose `torch_long()`
-  #' It's because module `nn_embedding` treats each categorical column as a vector
-  #' of indices.
-  if (!is.null(categorical_features)) {
-    X_tensor_cat <-
-      new_data %>%
-      select(!!key, !!index, !!categorical_features) %>%
-      arrange(!!key, !!index) %>%
-      as_tensor(!!key, !!index, dtype = torch_long())
-  }
-
-  X_tensor_numeric <-
-    new_data %>%
-    select(-!!categorical_features) %>%
-    arrange(!!key, !!index) %>%
-    as_tensor(!!key, !!index, dtype = torch_float())
-
-  tesnor_fcast <- obj$neural_network(X_tensor_cat, X_tensor_numeric)
+predict_recurrent_impl <- function(obj, new_data, key, index, categorical_features = NULL){
+  input_tensors <- resolve_data(new_data, key, index, categorical_features)
+  X_tensor_numeric <- input_tensors[[1]]
+  X_tensor_cat     <- input_tensors[[2]]
+  tesnor_fcast  <- obj$neural_network(X_tensor_cat, X_tensor_numeric)
   as.vector(as.array(tesnor_fcast))
 }
