fab3bba0e0ac8c3c4edb5fb545679d7b9c96b411
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2021-11-03 09:31:58 +0100

    Update metrics.R

--- R/metrics.R
+++ R/metrics.R
@@ -26,6 +26,38 @@ nnf_mape <- function(input, target){
   mean(abs((target - input) / target))
 }
 
+
+#' Symmetric Mean Absolute Percentage Error
+#'
+#' @param input (`torch_tensor`) A tensor of actual values
+#' @param target (`torch_tensor`) A tensor with the same shape as the input
+#'
+#' @details
+#' Computed according to the formula:
+#' \deqn{SMAPE = \frac{1}{n}\displaystyle\sum_{t=1}^{n} \frac{\left\|input - target\right\|}{(\left\|target\right\| + \left\|input\right\|) *0.5}}
+#'
+#' @seealso
+#' [yardstick::smape]
+#'
+#' @examples
+#' input <- c(92, 6.5, 57.69, 15.9, 88.47, 75.01, 5.06, 45.95, 27.8, 70.96)
+#' input <- as_tensor(input)
+#'
+#' target <- c(91.54, 5.87, 58.85, 10.73, 81.47, 75.39, 2.05, 40.95, 27.34, 66.61)
+#' target <- as_tensor(target)
+#'
+#' nnf_smape(input, target)
+#'
+#' @export
+nnf_smape <- function(input, target){
+    # Verify and change: input vs target
+    # Target = actual values
+    numerator   <- abs(input - target)
+    denominator <- ((abs(target) + abs(input)) / 2)
+    mean(numerator / denominator) 
+}
+
+
 #' Mean Absolute Scaled Error
 #'
 #' @param input (`torch_tensor`) A tensor of actual values
