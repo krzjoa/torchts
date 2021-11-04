2c83dec0877a911780baaa0af13160486fc0f0f8
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2021-11-04 10:38:24 +0100

    MASE added

--- R/metrics.R
+++ R/metrics.R
@@ -1,6 +1,6 @@
 # Metrics
 
-#' Mean Absolute Percentage Error
+#' Mean absolute percentage error
 #'
 #' @param input (`torch_tensor`) A tensor of actual values
 #' @param target (`torch_tensor`) A tensor with the same shape as the input
@@ -27,7 +27,7 @@ nnf_mape <- function(input, target){
 }
 
 
-#' Symmetric Mean Absolute Percentage Error
+#' Symmetric mean absolute percentage error
 #'
 #' @param input (`torch_tensor`) A tensor of actual values
 #' @param target (`torch_tensor`) A tensor with the same shape as the input
@@ -58,7 +58,7 @@ nnf_smape <- function(input, target){
 }
 
 
-#' Mean Absolute Scaled Error
+#' Mean absolute error
 #'
 #' @param input (`torch_tensor`) A tensor of actual values
 #' @param target (`torch_tensor`) A tensor with the same shape as the input
@@ -83,6 +83,42 @@ nnf_smape <- function(input, target){
 #' @export
 nnf_mae <- function(input, target){
   mean(abs(target - input))
+}
+
+
+#' Mean absolute scaled error
+#'
+#' @param input (`torch_tensor`) A tensor of actual values
+#' @param target (`torch_tensor`) A tensor with the same shape as the input
+#'
+#' @details
+#'
+#' Computed according to the formula:
+#' \deqn{MASE = \displaystyle\frac{MAE_{out-of-sample}}{\frac{1}{n-1}\sum_{i=2}^{n}\left\|a_i - a_{i-1}\right\|} }
+#'
+#' @seealso
+#' [yardstick::mase]
+#'
+#' @references  
+#' [Rob J. Hyndman (2006). ANOTHER LOOK AT FORECAST-ACCURACY METRICS 
+#'  FOR INTERMITTENT DEMAND. _Foresight_, 4, 46.](https://robjhyndman.com/papers/foresight.pdf)
+#'
+#' @examples
+#' input <- c(92, 6.5, 57.69, 15.9, 88.47, 75.01, 5.06, 45.95, 27.8, 70.96)
+#' input <- as_tensor(input)
+#'
+#' target <- c(91.54, 5.87, 58.85, 10.73, 81.47, 75.39, 2.05, 40.95, 27.34, 66.61)
+#' target <- as_tensor(target)
+#'
+#' nnf_mae(input, target)
+#'
+#' @export
+nnf_mase <- function(y_pred, target, in_sample_actual){
+  numerator   <- mean(abs(y_pred - target))
+  # In-sample naive forecast
+  diffs <- (in_sample_actual[:, :-1] - abs(in_sample_actual[:, 1:]))
+  denominator <- mean(diffs)
+  numerator / denominator 
 }
 
 
