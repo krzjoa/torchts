fcf084a220419a3cf7ce0467ab813e22c6bc5121
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2021-11-05 12:56:26 +0100

    embedding_size rules of thumb

--- R/categorical.R
+++ R/categorical.R
@@ -46,3 +46,51 @@ dict_size <- function(data){
   sapply(as.data.frame(data)[cols], dplyr::n_distinct)
 }
 
+#' @name embedding_size
+#' 
+#' Propose the length of embedding vector for each embedded feature.
+#' 
+#' @param x (`integer`) A vector with dictionary size for each feature
+#' @param 
+#' 
+#' @description 
+#' These functions returns proposed embedding sizes for each categorical feature.
+#' They are "rule of thumbs", so the are based on empirical rather than theoretical conclusions, 
+#' and their parameters can look like "magic numbers". Nevertheless, when you don't know what embedding size
+#' will be "optimal", it's good to start with such kind of general rules.
+#' 
+#' * **google**
+#' Proposed on the [Google Developer](https://developers.googleblog.com/2017/11/introducing-tensorflow-feature-columns.html) site
+#' \deqn{x^0.25}
+#' 
+#' * **fastai**
+#'  \deqn{1.6 * x^0.56}
+#' 
+#' 
+#' @return Proposed embedding sizes. 
+#' 
+#' @examples 
+#' dict_sizes <- dict_size(tiny_m5)
+#' embedding_size_google(dict_sizes)
+#' embedding_size_fastai(dict_sizes)
+#' 
+#' @references 
+#' 
+#' * [Introducing TensorFlow Feature Columns](https://developers.googleblog.com/2017/11/introducing-tensorflow-feature-columns.html)
+#' * [fastai - embedding size rule of thumb](https://github.com/fastai/fastai/blob/master/fastai/tabular/model.py)
+#' 
+#' 
+NULL
+
+#' @rdname embedding_size
+#' @export
+embedding_size_google <- function(x, max_size = 100){
+  pmin(ceiling(x ** 0.25), max_size)
+}
+
+#' @rdname embedding_size
+#' @export
+embedding_size_fastai <- function(x, max_size = 100){
+  pmin(round(1.6 * x ** 0.56), max_size)
+}
+
